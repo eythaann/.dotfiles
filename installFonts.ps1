e672922a8afde6a77d9aae5bab391f69723bebd1
eythaann <eythan.cvt@gmail.com>
2023-10-16 20:55:48 -0500

    add fonts

---
+++ installFonts.ps1
@@ -0,0 +1,2 @@
+#Copy-item -Force -Recurse -Verbose "$env:USERPROFILE\.dotfiles\fonts" -Destination "C:\Windows\Fonts"
+# TODO fix this task
--- setup.ps1
+++ setup.ps1
@@ -1,8 +1,11 @@
 Write-Host "`Creating links to .dotfiles"
 powershell -ExecutionPolicy Bypass -File "$env:USERPROFILE\.dotfiles\createLinks.ps1"
 
-Write-Host "`Installing apps..."
+Write-Host "`Installing apps"
 powershell -ExecutionPolicy Bypass -File "$env:USERPROFILE\.dotfiles\installCoreApps.ps1"
 
 Write-Host "`Installing new Shell and customizations"
 powershell -ExecutionPolicy Bypass -File "$env:USERPROFILE\.dotfiles\installShellCustomisations.ps1"
+
+Write-Host "`Installing fonts"
+powershell -ExecutionPolicy Bypass -File "$env:USERPROFILE\.dotfiles\installFonts.ps1"
