c6fc8839b859e08fbd1b22dfc7803cc541ab1e00
eythaann <eythan.cvt@gmail.com>
2023-10-16 21:43:55 -0500

    update

--- installCoreApps.ps1
+++ installCoreApps.ps1
@@ -1,18 +1,21 @@
 # fancyWM for some shortcuts and title windows managment.
-Write-Host "\nInstaling FancyWM"
+Write-Host "`nInstaling FancyWM"
 winget install "fancywm" -s msstore
 
 # AHK (auto hot key) for the rest of the shortcuts.
-Write-Host "\nInstaling Auto Hot key"
+Write-Host "`nInstaling Auto Hot key"
 winget install "AutoHotkey" -s winget
 
-Write-Host "\nInstaling VsCode"
+Write-Host "`nInstaling VsCode"
 winget install Microsoft.VisualStudioCode -s winget
 
-Write-Host "\nInstaling Firefox!!!"
+Write-Host "`nInstaling Firefox!!!"
 winget install "Mozilla Firefox" -s msstore
 winget install firefoxpwa
 
-Write-Host "\nInstaling Other Programs"
-winget install NordPass
+Write-Host "`nInstaling NordPass"
+winget settings --enable InstallerHashOverride
+winget install NordSecurity.NordPass --ignore-security-hash
+
+Write-Host "`nInstaling Steam"
 winget install Valve.Steam
