5de8812dae94aa26684c632144ac96a8d19acacc
eythaann <eythan.cvt@gmail.com>
2025-07-13 16:04:10 -0500

    save

--- linux/debian/.bashrc
+++ linux/debian/.bashrc
@@ -1,3 +1,4 @@
+# only run this code if the shell is interactive
 case $- in
     *i*) ;;
       *) return;;
@@ -5,4 +6,15 @@ esac
 
 alias ll='ls -lA'
 
+alias g='git'
+alias gcm='git commit -m'
+alias gca='git commit --amend --no-edit'
+alias gps='git push'
+alias gpsf='git push --force --no-verify'
+
+alias dotfiles='code ~/.dotfiles'
+
+alias reload-shell='source ~/.bashrc'
+
 eval "$(starship init bash)"
+clear
--- linux/debian/setup.sh
+++ linux/debian/setup.sh
@@ -4,6 +4,10 @@ sudo apt install -y \
   zip \
   unzip \
 
+# configuring git & github
+mkdir ~/.dotfiles/linux/debian/.ssh
+ln -sf ~/.dotfiles/linux/debian/.ssh ~/.ssh
+
 # install kubectl
 curl -LO /tmp/kubectl "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 sudo install -o root -g root -m 0755 /tmp/kubectl /usr/local/bin/kubectl
@@ -16,3 +20,7 @@ curl -sS https://starship.rs/install.sh | sh
 ln -sf ~/.dotfiles/linux/debian/.bashrc ~/.bashrc
 ln -sf ~/.dotfiles/linux/debian/.config ~/.config
 ln -sf ~/.dotfiles/linux/debian/.config/git/config ~/.gitconfig
+
+# rust
+sudo apt install -y build-essential
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
